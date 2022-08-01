4f663d260ded5bb6061042837cf79acd3869eb8d
code1218 <skjil1218@gmail.com>
2022-08-01 21:07:43 +0900

    리스트 불러오기 전

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,14 @@
+const selectedTypeButton = document.querySelector(".selected-type-button");
+const typeSelectBoxList = document.querySelector(".type-select-box-list");
+
+selectedTypeButton.onclick = () => {
+    typeSelectBoxList.classList.toggle("visible");
+}
+
+selectedTypeButton.onblur = () => {
+    typeSelectBoxList.classList.toggle("visible");
+}
+
+function load() {
+	
+}
